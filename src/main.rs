287529120afba70c55af2f494789a8631943888e
derrick921213 <dlin12457@gmail.com>
2024-04-20 11:58:34 +0800

    添加pass2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,22 @@ use line::*;
 use optables::*;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+struct FileWriter {
+    writer: BufWriter<File>,
+}
+impl FileWriter {
+    fn new(path: &str) -> io::Result<Self> {
+        let file = File::create(path)?;
+        let writer = BufWriter::new(file);
+        Ok(Self { writer })
+    }
+    fn write(&mut self, content: &str) -> io::Result<()> {
+        self.writer.write_all(content.as_bytes())?;
+        self.writer.flush()?;
+        Ok(())
+    }
+}
 
 fn main() -> io::Result<()> {
     let mut args = std::env::args();
@@ -30,7 +45,7 @@ fn main() -> io::Result<()> {
                 if let Some(index) = line.find('.') {
                     line = line[..index].trim_end().to_string();
                 }
-                let mut tokens = line.split('\t').collect::<Vec<&str>>();
+                let mut tokens = line.split(['\t', ',']).collect::<Vec<&str>>();
                 if tokens.len() > 3 {
                     tokens.truncate(3);
                 }
@@ -133,22 +148,141 @@ fn main() -> io::Result<()> {
                     }
                 }
             }
-            for line in line_struct.iter() {
-                println!(
-                    "{:06}\t{:8}\t{:8}\t{:8}",
-                    line.get_memory(),
-                    line.get_symbol().unwrap_or(&"".to_string()),
-                    line.get_op(),
-                    line.get_operand1().unwrap_or(&"".to_string())
-                )
-            }
             let total = end - start;
-            println!("\nProgram length = {}\n", total);
-            for (k, v) in symbol_table.iter() {
-                println!("{:10}: {:06}", k, v);
-            }
+            // Pass1
+            // =========
+            // for line in line_struct.iter() {
+            //     println!(
+            //         "{:06X}\t{:8}\t{:8}\t{:8}",
+            //         line.get_memory(),
+            //         line.get_symbol().unwrap_or(&"".to_string()),
+            //         line.get_op(),
+            //         line.get_operand1().unwrap_or(&"".to_string())
+            //     )
+            // }
+            // println!("\nProgram length = {:06X}\n", total);
+            // for (k, v) in symbol_table.iter() {
+            //     println!("{:10}: {:06X}", k, v);
+            // }
+            // println!("{:#?}", line_struct);
+            // =========
+            pass2(&line_struct, &symbol_table, total)?;
         }
         None => println!("Usage: {} <filename.asm>", program),
     }
+
+    Ok(())
+}
+
+#[allow(unused_assignments)]
+fn pass2(
+    line_struct: &[Line],
+    symbol_table: &HashMap<String, u32>,
+    total_len: u32,
+) -> io::Result<()> {
+    let mut file_writer = FileWriter::new("output.txt")?;
+    let mut end_address = 0x0;
+    if let Some(start_line) = line_struct.iter().find(|l| l.get_op() == "START") {
+        let empty = "".to_string();
+        let program_name = start_line.get_symbol().unwrap_or(&empty);
+        let start_address = start_line.get_memory();
+        end_address = start_address;
+        let header_record = format!(
+            "H{:<6}{:06X}{:06X}\n",
+            program_name, start_address, total_len
+        );
+        file_writer.write(&header_record)?;
+    } else {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "No START directive found",
+        ));
+    }
+    let mut text_record = String::new();
+    let mut text_record_length = 0;
+    let mut start_address = 0;
+
+    for line in line_struct.iter() {
+        if let FormatDirective::Format(Format::FMT0) = line.get_fmt() {
+            continue;
+        }
+
+        let mut object_code = String::new();
+        let ni = match line.get_address_mode() {
+            AddrMode::Simple => 0x3,
+            AddrMode::Immediate => 0x1,
+            AddrMode::Indirect => 0x2,
+            AddrMode::Index => 0x3,
+        };
+        let x_bit = if matches!(line.get_address_mode(), AddrMode::Index) {
+            0x8
+        } else {
+            0x0
+        };
+
+        match line.get_fmt() {
+            FormatDirective::Format(Format::FMT1) => {
+                object_code = format!("{:02X}", line.get_code());
+            }
+            FormatDirective::Format(Format::FMT2) => {
+                let r1 = line
+                    .get_operand1()
+                    .map_or(0, |op| symbol_table.get(op).copied().unwrap_or(0) as u16);
+                let r2 = line
+                    .get_operand2()
+                    .map_or(0, |op| symbol_table.get(op).copied().unwrap_or(0) as u16);
+                object_code = format!("{:02X}{:1X}{:1X}", line.get_code(), r1, r2);
+            }
+            FormatDirective::Format(Format::FMT3_4) => {
+                let extended = matches!(line.get_fmt(), FormatDirective::Format(Format::FMT4));
+                let pc = line.get_memory() + if extended { 4 } else { 3 };
+                let mut displacement = 0;
+                if let Some(symbol) = line.get_operand1() {
+                    if let Some(&address) = symbol_table.get(symbol) {
+                        displacement = if extended {
+                            address
+                        } else {
+                            address.wrapping_sub(pc) & 0xFFF
+                        };
+                    } else {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidData,
+                            format!("Undefined symbol {}", symbol),
+                        ));
+                    }
+                }
+                let mut xbpe = if extended { 0x1 } else { 0x2 };
+                xbpe |= x_bit;
+                let opcode = line.get_code() as u32;
+                object_code = format!("{:02X}{:01X}{:03X}", opcode, ni << 4 | xbpe, displacement);
+            }
+            _ => {}
+        }
+
+        if text_record_length + object_code.len() / 2 > 30 {
+            file_writer.write(&format!(
+                "T{:06X}{:02X}{}\n",
+                start_address, text_record_length, text_record
+            ))?;
+            text_record.clear();
+            text_record_length = 0;
+            start_address = line.get_memory();
+        }
+
+        if text_record.is_empty() {
+            start_address = line.get_memory();
+        }
+
+        text_record_length += object_code.len() / 2;
+        text_record.push_str(&object_code);
+    }
+
+    if !text_record.is_empty() {
+        file_writer.write(&format!(
+            "T{:06X}{:02X}{}\n",
+            start_address, text_record_length, text_record
+        ))?;
+    }
+    file_writer.write(&format!("E{:06X}\n", end_address))?;
     Ok(())
 }
